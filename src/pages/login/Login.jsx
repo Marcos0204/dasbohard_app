5db36e400a0a596f97063955850992c4309dcf0e
Marcos Bastidas <marcosbastidas301@gmail.com>
2022-04-01 23:31:13 +0200

    update-login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import "./login.scss"
 
 const Login = () => {
   const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
   const halendLogin = (e) =>{
     e.prventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(true)
+        // ..
+      });
+
   }
   return (
     <div className='login'>
       <form onSubmit={halendLogin}>
-        <input type='email' placeholder='email'/>
-        <input type='password' placeholder='password'/>
+        <input type='email' placeholder='email' onChange={e => setEmail(e.target.value)}/>
+        <input type='password' placeholder='password' onChange={e => setPassword(e.target.value)}/>
         <button type='submit'>login</button>
         {error && <span>Wrong Email or Password!</span>}
       </form>
