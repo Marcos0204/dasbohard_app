7a3eee1f0bd341c43339e95748d0ec19f671bf91
Marcos Bastidas <marcosbastidas301@gmail.com>
2022-04-04 17:39:16 -0400

    img_uploaded

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { setDoc, doc, serverTimestamp  } from "firebase/firestore";
 import { createUserWithEmailAndPassword  } from "firebase/auth";  
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from '../../firebase';
+import { auth, db, storage } from '../../firebase';
 
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
@@ -14,7 +15,47 @@ import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUpload
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [data, setData] = useState({});
+  const [per, setPerc] = useState(null);
+  console.log(per)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const uploadFile = () => {
+      //const name = new Date().getTime() + file.name;
+      //console.log(name);
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          setPerc(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev) => ({ ...prev, img: downloadURL }));
+          });
+        }
+      );
+    };
+    file && uploadFile();
+  }, [file]);
 
   const handleInput = (e) => {
     const id = e.target.id;
